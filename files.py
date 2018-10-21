1e0065edcdfe6563b333245eb3114aa4ef1f44b4
Thusitha <thusitha1996@hotmail.fr>
2018-10-21 19:42:15 +0200

    add comments

--- files.py
+++ files.py
@@ -2,25 +2,27 @@ from pathlib import Path
 from datetime import datetime
 import chardet
 
+# Classe Fichier
 class File :
 
+    # Constructeur
     def __init__(self, path):
         self.path = path;
         self.pathFichier = Path(self.path);
 
+    # Retourner la taille du fichier
     def getTailleFichier(self) :
         tailleFichier = self.pathFichier.stat().st_size.__str__();
         return tailleFichier;
 
+    # Retourner la date de la dernière modification
     def getDateDerniereModification(self) :
         derniereModifTime = self.pathFichier.stat().st_mtime;
         derniereModifTimeFormat = datetime.fromtimestamp(derniereModifTime).__str__();
         return derniereModifTimeFormat;
 
+    # Retourner le type d'encodage
     def getTypeEncodage(self) :
         bytes = self.pathFichier.read_bytes();
         encoding = chardet.detect(bytes);
         return encoding.get("encoding");
-
-    def toString(self):
-        return self.getTailleFichier() + ", " + self.getDateDerniereModification() + ", " + self.getTypeEncodage();
