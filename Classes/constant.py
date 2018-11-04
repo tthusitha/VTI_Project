ce3d6b98d6bfd13112995868ee352e71272418ef
MassHD <mass.ha@live.fr>
2018-11-04 21:32:51 +0100

    Update Classes/constant.py

--- Classes/constant.py
+++ Classes/constant.py
@@ -1,21 +1,41 @@
 # Classe contenant les constantes
 class Constant :
 
+    #Paramètre pour choisir le Chemin du fichier
     PARAM_CHEMIN_FICHIER = "-f";
+    #Paramètre pour l'interaction avec l'utilisateur
     PARAM_INTERACTION_UTILISATEUR = "-u";
+    #Paramètre du chemin du fichier de log
     PARAM_LOG = "-l";
+    #Type des variables qualitatives
     TYPE_QUALITATIVE_OBJECT = "object";
+    #Type des variables quantitatives
     TYPE_QUANTITATIVE_INT = "int64";
+    #Type des variables quantitatives
     TYPE_QUANTITATIVE_FLOAT = "float64";
+    #Paramètre pour afficher info fichier
     PARAM_INFO_FICHIER = "f";
+    #Paramètre pour afficher les infos des données
     PARAM_INFO_DONNEES = "d";
+    #Quitter l'application
     PARAM_QUITTER = "q";
+    #Pour afficher la taille du fichier
     PARAM_TAILLE_FICHIER ="t";
+    #Pour afficher l'encodage du fichier
     PARAM_ENCODAGE_FICHIER = "e";
+    #Pour afficher la derniere date de modification du fichier
     PARAM_DERNIERE_MODIF_FICHIER = "d";
+    #Pour afficher toutes les informations d'un fichier
     PARAM_TOUT_INFOS_FICHIER = "a";
+    #Pour revenir en arrière
     PARAM_RETOUR = "r";
+    #Pour afficher les informations des données de la table
     PARAM_INFOS_TABLE = "b";
+    #Pour afficher les informations sur les vairables quantitatives
     PARAM_INFOS_QUANTITATIVE = "v";
+    #Pour afficher les informations sur les variables qualitatives
     PARAM_INFOS_QUALITATIVE = "l";
+    #Si l'utilisateur veut générer les plots de distribution
     PARAM_PLOT = "p";
+    #Chemin du fichier de log
+    CHEMIN_FICHIER_LOG = "log.txt";
